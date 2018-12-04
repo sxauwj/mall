8afabd677bff8ba1bf9fd4ba9057d0817733efd5
sxauwj <1328330059@qq.com>
2018-12-04 20:56:42 +0800

    settings添加导包路径

--- meiduo/meiduo/settings/dev.py
+++ meiduo/meiduo/settings/dev.py
@@ -14,7 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+import sys
+# sys.path==>表示python解释器查找包的路径
+sys.path.insert(0,os.path.join(BASE_DIR,'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
