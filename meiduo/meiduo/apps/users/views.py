0f79829e6c4cecec6a5b1c8eaa764def0cb281fc
sxauwj <1328330059@qq.com>
2018-12-05 17:56:34 +0800

    补充前端文件

--- meiduo/meiduo/apps/users/views.py
+++ meiduo/meiduo/apps/users/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import User
+from rest_framework.response import Response
 
 
+class UsernameCountView(APIView):
+    """用户名数量"""
+    def get(self, requeset, username):
+        count = User.objects.filter(username=username).count()
+        data = {
+            'username': username,
+            'count': count
+        }
+        return Response(data)
+
+class MobileCountView(APIView):
+    """手机号数量"""
+    def get(self,request, mobile):
+        count = User.objects.filter(mobile=mobile).count()
+        data = {
+            'mobile':mobile,
+            'count':count
+        }
+
+        return Response(data)
