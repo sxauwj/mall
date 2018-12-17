7b414be3f525b71231ebc56cc61cbfb557009718
sxauwj <1328330059@qq.com>
2018-12-17 23:57:02 +0800

    保存订单

--- meiduo/meiduo/apps/orders/urls.py
+++ meiduo/meiduo/apps/orders/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^settlement/$',views.SettlementView.as_view())
+    url('^settlement/$',views.SettlementView.as_view()),
+    url('^$',views.OrderView.as_view()),
 ]
